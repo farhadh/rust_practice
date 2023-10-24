c7f00dc870d3d0e801da0cfe0f4645c60f4f7854
farhadh <farhad.21.7@gmail.com>
2023-10-24 15:27:36 +0200

    Initial projects

---
+++ variables/src/main.rs
@@ -0,0 +1,52 @@
+/*
+if we write the code like this:
+`
+let mut a = 0;
+let b = &a;
+`
+it means that `b` is a reference to `a`. In this case:
+- `b` CANNOT be changed/pointed to something/somewhere else because `b` is immutable.
+- `a` CANNOT be changed through `b` because `b` has borrowed `a` as an immutable reference.
+
+
+if we write the code like this:
+`
+let mut a = 0;
+let b = &mut a;
+`
+it means that `b` is a reference to `a`. In this case:
+- `b` cannot be changed/pointed to something/somewhere else because `b` is immutable.
+- `a` CAN be changed through `b` because `b` has borrowed `a` as an mutable reference.
+
+
+if we write the code like this:
+`
+let mut a = 0;
+let mut b = &mut a;
+`
+it means that `b` is a reference to `a`. In this case:
+- `b` CAN be changed/pointed to something/somewhere else because `b` is mutable.
+- `a` CAN be changed through `b` because `b` has borrowed `a` as an mutable reference.
+
+*/
+
+fn main() {
+    let a = 0;
+    println!("a: {}, &a: {:p}", a, &a);
+
+    let x = 1;
+    println!("x: {}, &x: {:p}", x, &x);
+
+
+    let b = &a;
+        
+    println!("b: {}, &b: {:p}", b, &b);
+
+    /*
+    In Rust, variables are immutable by default, but when you use the let keyword to create a new variable with the same name, it effectively shadows the previous variable, and the new variable can have a different type or be mutable if you explicitly specify it.
+    */
+    b = &x;
+        
+    println!("b: {}, &b: {:p}", b, &b);
+    
+}

6b5922495dd28ae3500b781f6c335703bd9800b4
farhadh <farhad.21.7@gmail.com>
2024-09-24 14:30:52 +0200

    more explanations on associated functions

--- methods_and_associated-functions/src/main.rs
+++ methods_and_associated-functions/src/main.rs
@@ -8,6 +8,22 @@ However, they can be useful to group some utility function together with a class
 They could be put outside the class, but grouping them inside the class may make sense where they are only applicable there.
 
 You can also reference the method via an instance or the class, rather than the module name, which may help the reader understand to what instance the method is related.
+
+Associated functions that aren’t methods are often used for constructors that will return a new instance of the struct. These are often called new, but new isn’t a special name and isn’t built into the language. For example, we could choose to provide an associated function named square that would have one dimension parameter and use that as both width and height, thus making it easier to create a square Rectangle rather than having to specify the same value twice:
+
+Filename: src/main.rs
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+the 2nd self (`Self { width...`) refers to the type that is implemented on (which is the `Rectangle`). In other words, the `Self` keywords in the return type and in the body of the function are aliases for the type that appears after the `impl` keyword, which in this case is `Rectangle`.
+
 */
 
 
